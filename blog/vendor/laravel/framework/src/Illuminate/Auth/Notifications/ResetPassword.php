ecd9c3e1749bc529a351c8b15835d38c6472ef03
tylergong <tylergong@163.com>
2017-07-09 18:45:59 +0800

    邮件模板修改

--- blog/vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
+++ blog/vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -45,8 +45,13 @@ class ResetPassword extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->line('You are receiving this email because we received a password reset request for your account.')
+			->line('You are receiving this email because we received a password reset request for your account.')
+            ->line('您收到了这封邮件,是因为我们收到了来自您账户的密码重置请求.')
             ->action('Reset Password', url(config('app.url').route('password.reset', $this->token, false)))
-            ->line('If you did not request a password reset, no further action is required.');
+			->line('If you did not request a password reset, no further action is required.')
+			->line('如果您没有要求密码重置，则无需要理会当前操作')
+			->line('Thanks!')
+			->line('谢谢!')
+			->salutation(config('app.name').'账户团队');
     }
 }
